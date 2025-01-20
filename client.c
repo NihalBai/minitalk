552b14394a127f0f0af8c174bc9ecc66bb0e1508
Nihal Baidaoui <nbaidaou@c3r1p3.1337.ma>
2025-01-20 18:38:17 +0100

    ver final

--- client.c
+++ client.c
@@ -3,66 +3,109 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nbaidaou <nbaidaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 10:20:00 by root              #+#    #+#             */
-/*   Updated: 2025/01/16 17:36:51 by root             ###   ########.fr       */
+/*   Updated: 2025/01/19 01:57:24 by nbaidaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
-volatile sig_atomic_t ack_received = 0; 
+volatile sig_atomic_t	ack_received = 0;
 
-void sign_handler(int sig) {
-    if (sig == SIGUSR1) {
-        ack_received = 1;  
-    }
-}
-
-void shift_send(char *s, int pid) {
-    int bit;
-    size_t i;
-    
-    i = 0;
-    while (i < strlen(s)) 
-    {
-        int j = 7;
-        while (j >= 0) 
-        {
-            bit = (s[i] >> j) & 1;  
-            if (bit == 0)
-                kill(pid, SIGUSR1);  
-            else
-                kill(pid, SIGUSR2);  
-            while (!ack_received)
-                usleep(100); 
-            ack_received = 0; 
-            j--;
-        }
-        i++;
-    }
-}
-
-int main(int ac ,char ** av)
+void	sign_handler(int sig)
 {
-    if(ac == 3)
-    {
-        int pid;
-        pid = atoi(av[1]);
-        struct sigaction sa;
-        sa.sa_handler = &sign_handler;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = 0;
-        if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-            perror("sigaction");
-            exit(EXIT_FAILURE);
-        }
-        shift_send(av[2],pid);
-        write(1,"finish\n",7);
-    }
+	if (sig == SIGUSR1)
+	{
+		ack_received = 1;
+	}
+}
+
+void	error(void)
+{
+	perror("kill");
+	exit(EXIT_FAILURE);
+}
+void	send(int bit, int pid)
+{
+	if (bit == 0)
+	{
+		if (kill(pid, SIGUSR1) == -1)
+			error();
+	}
+	else
+	{
+		if (kill(pid, SIGUSR2) == -1)
+			error();
+	}
+}
+void	shift_send(char *s, int pid)
+{
+	size_t			i;
+	int				bit;
+	unsigned char	byte;
+	int				j;
+
+	i = 0;
+	while (s[i])
+	{
+		byte = (unsigned char)s[i];
+		j = 7;
+		while (j >= 0)
+		{
+			bit = (byte >> j) & 1;
+			send(bit, pid);
+			while (!ack_received)
+				usleep(100);
+			ack_received = 0;
+			j--;
+		}
+		i++;
+	}
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	r;
+
+	i = 0;
+	sign = 1;
+	r = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || str[i] == '\v'
+		|| str[i] == '\n' || str[i] == '\f')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			sign = -1;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		r = r * 10 + (str[i++] - '0');
+	}
+	return (r * sign);
+}
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		int pid;
+		pid = ft_atoi(av[1]);
+		struct sigaction sa;
+		sa.sa_handler = &sign_handler;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = 0;
+		if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		{
+			perror("sigaction");
+			exit(EXIT_FAILURE);
+		}
+		shift_send(av[2], pid);
+		write(1, "finish\n", 7);
+	}
 }
--- server.c
+++ server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nbaidaou <nbaidaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 11:14:22 by root              #+#    #+#             */
-/*   Updated: 2025/01/16 17:41:07 by root             ###   ########.fr       */
+/*   Updated: 2025/01/19 01:55:16 by nbaidaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void handle_sig(int sig, siginfo_t *info, void *context) {
-    static char temp = 0;
-    static int count_bits = 0;
-    (void)context; // Unused parameter
+void	handle_sig(int sig, siginfo_t *info, void *context)
+{
+	static char	temp = 0;
+	static int	count_bits = 0;
 
-    temp <<= 1;
-    if (sig == SIGUSR2)
-        temp |= 1;
-
-    count_bits++;
-    if (count_bits == 8) {
-        if (temp == '\0') {
-            write(1, "\n", 1); // Print a newline at the end of the message
-        } else {
-            write(1, &temp, 1); // Print the received character
-        }
-        temp = 0;
-        count_bits = 0;
-    }
-
-    // Send acknowledgment to the client
-    kill(info->si_pid, SIGUSR1);
+	(void)context;
+	temp <<= 1;
+	if (sig == SIGUSR2)
+		temp |= 1;
+	count_bits++;
+	if (count_bits == 8)
+	{
+		if (temp == '\0')
+		{
+			write(1, "\n", 1);
+		}
+		else
+		{
+			write(1, &temp, 1);
+		}
+		temp = 0;
+		count_bits = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
 }
 
-int main() {
-    struct sigaction sa;
+int	main(void)
+{
+	struct sigaction	sa;
 
-    sa.sa_sigaction = &handle_sig;
-    sa.sa_flags = SA_SIGINFO;
-    sigemptyset(&sa.sa_mask);
-
-    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server PID: %d\n", getpid());
-
-    while (1) {
-        pause(); // Wait for incoming signals
-    }
-
-    return 0;
+	sa.sa_sigaction = &handle_sig;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) ==
+		-1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	printf("Server PID: %d\n", getpid());
+	while (1)
+	{
+		pause();
+	}
+	return (0);
 }
